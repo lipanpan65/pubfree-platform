5de161ca0805abab4e165bc62616966e983be85f
李盼盼 <ppli@M-KN-PPLI.local>
2025-07-03 20:22:40 +0800

    style: Pubfree Header styles

--- pubfree-web/src/index.tsx
+++ pubfree-web/src/index.tsx
@@ -7,16 +7,35 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import PageLayout from "@/components/layout/page-layout";
 
-import Routes from "./routes";
+import Routes from "@/routes";
 
 import "./style/style.less";
+
+// antd v5 主题配置
+const theme = {
+  token: {
+    colorPrimary: '#6247aa',
+    colorLink: '#6247aa',
+    colorTextHeading: '#6247aa',
+  },
+  components: {
+    Layout: {
+      headerBg: '#6247aa',
+      headerHeight: 50,
+    },
+    Menu: {
+      darkItemBg: '#6247aa',
+      darkItemSelectedBg: '#815ac0',
+    },
+  },
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
 root.render(
   <BrowserRouter>
-    <ConfigProvider locale={zhCN}>
+    <ConfigProvider locale={zhCN} theme={theme}>
       <PageLayout>
         <Routes />
       </PageLayout>
